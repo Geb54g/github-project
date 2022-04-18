9fb4b0b15a8d033e30a3fa0e0d8a3d1cce1a27a9
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-18 21:08:34 +0300

    st:pipe customization

--- src/app/pipes/date.pipe.ts
+++ src/app/pipes/date.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DatePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, ...args: unknown[]): unknown {
+    return value;
   }
 
 }
