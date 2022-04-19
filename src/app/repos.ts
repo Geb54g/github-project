6e4cc4fa187b686e1ed96ffe49f6e2ef3908e24b
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-04-19 19:22:59 +0300

    st:style repo section

--- src/app/repos.ts
+++ src/app/repos.ts
@@ -1,2 +1,9 @@
 export class Repos {
+    id:number
+    repo:string
+
+    constructor(id:number,repo:string){
+        this.id = id
+        this.repo =repo
+    }
 }
--- src/app/repositories/repositories.component.ts
+++ src/app/repositories/repositories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Repos } from '../repos';
 
 @Component({
   selector: 'app-repositories',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repositories.component.css']
 })
 export class RepositoriesComponent implements OnInit {
+      repositories:Repos[]=[
+        new Repos(1, 'https://github.com/Geb54g/schooolweb'),
+        new Repos(2, 'https://github.com/Geb54g/delanistudio'),
+        new Repos(3, 'https://github.com/Geb54g/burgerplace'),
+        new Repos(4, 'https://github.com/Geb54g/gebportfolio'),
+        new Repos(5, 'https://github.com/Geb54g/birthdaywed'),
+        new Repos(6, 'https://github.com/Geb54g/delanistudio')
+      ]
 
   constructor() { }
 
