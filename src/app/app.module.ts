17bce238201e7b7b9217f161435fe26d715eb6f6
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-01 12:36:16 +0300

    ft:add a search bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PersonalDetailsComponent } from './personal-details/personal-details.component';
-import { LoginComponent } from './login/login.component';
 import { DatePipe } from './pipes/date.pipe';
-import { RepositoriesComponent } from './repositories/repositories.component';
-import { FontDirective } from './font.directive';
-import { UsersComponent } from './users/users.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileService } from './service/profile.service';
+import { GithubComponent } from './github/github.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    NavbarComponent,
-    PersonalDetailsComponent,
-    LoginComponent,
     DatePipe,
-    RepositoriesComponent,
-    FontDirective,
-    UsersComponent
+    ProfileComponent,
+    GithubComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/repos.ts
+++
@@ -1,9 +0,0 @@
-export class Repos {
-    id:number
-    repo:string
-
-    constructor(id:number,repo:string){
-        this.id = id
-        this.repo =repo
-    }
-}
