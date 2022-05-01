fd57cb04a1b30a0bd0f8db947583c2f865ae31a8
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-05-01 19:45:29 +0300

    ft:added http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DatePipe } from './pipes/date.pipe';
 import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileService } from './service/profile.service';
 import { GithubComponent } from './github/github.component';
-
-
+import {HttpClientModule} from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +17,9 @@ import { GithubComponent } from './github/github.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
--- src/app/service/profile.service.ts
+++ src/app/service/profile.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+    url : string ="https://api.github.com/users/";
+  constructor(private http:HttpClient) { }
+
+  getUser(username: string){
+     
+    this.http.get(this.url + username);
+  }
 }
